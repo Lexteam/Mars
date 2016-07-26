ebbc80a561733d2b563705623222ef466d99de7e
Jamie Mansfield <dev@jamierocks.uk>
2016-07-26 21:10:10 +0100

    Initial commit

---
+++ src/main/java/xyz/lexteam/mars/compiler/CompilerMain.java
@@ -0,0 +1,12 @@
+package xyz.lexteam.mars.compiler;
+
+/**
+ * The main class for the Mars compiler.
+ */
+public final class CompilerMain {
+
+    public static void main(String[] args) {
+
+    }
+
+}
